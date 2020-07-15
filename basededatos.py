641961e3a6576e97d3d2410e3cf32568ae9f3523
Ivan Ferro <ivanferroherbon@gmail.com>
2020-07-15 18:01:57 +0200

    BD update

--- basededatos.py
+++ basededatos.py
@@ -5,4 +5,30 @@ MONGO_URI = "mongodb://localhost"
 client = MongoClient(MONGO_URI)
 db = client['TFG']
 usuario = db['DIRECCIONES']
-usuario.insert_one({"_id":"ejemplo","IPPrivada":"192.168.0.5","IPPublica":"1.1.1.1"})
+##usuario.insert_one({"_id":"MAC","IPPrivada":"192.168.0.5","IPPublica":"1.1.1.1"})
+
+
+#mostrar todos
+for document in usuario.find({}):
+    print(document)
+
+#mostar o buscado
+doc = usuario.find_one({
+    "IPPublica": "1.1.1.1"
+})
+print(doc)
+
+##update
+usuario.update_one({
+    "IPPublica":"1.1.1.1"
+    }, {
+    "$set":{
+        "IPPrivada":"This IP bitch"
+    }
+    })
+
+#mostar o buscado
+doc = usuario.find_one({
+    "IPPublica": "1.1.1.1"
+})
+print(doc)
